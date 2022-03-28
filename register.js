895780b78a7255cef4c01331a54ba8b58ea1bce5
LLla6aJl <salimovvvv@mail.ru>
2022-03-28 09:47:46 +0300

    Create register.js

---
+++ register.js
@@ -0,0 +1,23 @@
+//register
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "user": {
+    "username": "Rustam",
+    "email": "salimovvvv@mail.ru",
+    "password": "salim"
+  }
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://kata.academy:8021/api/users", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
